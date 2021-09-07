eaf98398bcb6cf9145a7817f8dd5f89371df8a01
thomaszub <thomas.zub@outlook.de>
2021-09-07 19:55:32 +0200

    Ignore error explicitly

--- cmd/root.go
+++ cmd/root.go
@@ -60,6 +60,6 @@ func initConfig() {
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+		_, _ = fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
 }
