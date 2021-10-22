9c1d13b1fc6429198724c88deeb442564d33e573
Thomas Zub <thomas.zub@codecentric.de>
2021-10-22 08:16:49 +0200

    Removed config file code, comment cleanup

--- cmd/ls.go
+++ cmd/ls.go
@@ -20,17 +20,8 @@ var lsCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(lsCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// lsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// lsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	lsCmd.Flags().StringP("formatter", "f", "del,=", "Specifies the formatter (currently only del) with a comma separated list of configuration arguments")
 	lsCmd.Flags().BoolP("all", "a", false, "Show hidden environmental variables (starting with _) ")
+	lsCmd.Flags().StringP("formatter", "f", "del,=", "Specifies the formatter (currently only del) with a comma separated list of configuration arguments")
 }
 
 func lsMain(cmd *cobra.Command, _ []string) error {
