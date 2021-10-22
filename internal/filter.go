e26667fd314194c0e1835189a8327caaec639805
Thomas Zub <thomas.zub@codecentric.de>
2021-10-22 08:43:46 +0200

    Moved FilterHandler to own file

--- internal/filter.go
+++ internal/filter.go
@@ -17,34 +17,3 @@ func (f *NoPrefixFilter) Accept(env EnvVar) bool {
 func NewNoPrefixFilter(prefix string) Filter {
 	return &NoPrefixFilter{prefix: prefix}
 }
-
-type FilterHandler struct {
-	filters []Filter
-}
-
-func (f *FilterHandler) Accepted(envs []EnvVar) []EnvVar {
-	accepted := make([]EnvVar, 0)
-	for _, env := range envs {
-		if f.Accept(env) {
-			accepted = append(accepted, env)
-		}
-	}
-	return accepted
-}
-
-func (f *FilterHandler) Accept(env EnvVar) bool {
-	for _, filter := range f.filters {
-		if !filter.Accept(env) {
-			return false
-		}
-	}
-	return true
-}
-
-func (f *FilterHandler) AppendFilter(filter Filter) {
-	f.filters = append(f.filters, filter)
-}
-
-func NewEmptyFilterHandler() FilterHandler {
-	return FilterHandler{filters: make([]Filter, 0)}
-}
---
+++ internal/filter_handler.go
@@ -0,0 +1,32 @@
+package internal
+
+type FilterHandler struct {
+	filters []Filter
+}
+
+func (f *FilterHandler) Accepted(envs []EnvVar) []EnvVar {
+	accepted := make([]EnvVar, 0)
+	for _, env := range envs {
+		if f.Accept(env) {
+			accepted = append(accepted, env)
+		}
+	}
+	return accepted
+}
+
+func (f *FilterHandler) Accept(env EnvVar) bool {
+	for _, filter := range f.filters {
+		if !filter.Accept(env) {
+			return false
+		}
+	}
+	return true
+}
+
+func (f *FilterHandler) AppendFilter(filter Filter) {
+	f.filters = append(f.filters, filter)
+}
+
+func NewEmptyFilterHandler() FilterHandler {
+	return FilterHandler{filters: make([]Filter, 0)}
+}
