9e10fc66bf86702b45c8bf4d0f83af3358f58aa8
Thomas Zub <thomas.zub@codecentric.de>
2021-10-22 09:36:25 +0200

    Added RegexFilter

--- internal/filter.go
+++ internal/filter.go
@@ -1,6 +1,9 @@
 package internal
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
 type Filter interface {
 	Accept(env EnvVar) bool
@@ -10,10 +13,23 @@ type NoPrefixFilter struct {
 	prefix string
 }
 
+type RegexFilter struct {
+	regex *regexp.Regexp
+}
+
 func (f *NoPrefixFilter) Accept(env EnvVar) bool {
 	return !strings.HasPrefix(env.Name, f.prefix)
 }
 
 func NewNoPrefixFilter(prefix string) Filter {
 	return &NoPrefixFilter{prefix: prefix}
+}
+
+func (f *RegexFilter) Accept(env EnvVar) bool {
+	accept := f.regex.MatchString(env.Name)
+	if accept {
+		return true
+	}
+	accept = f.regex.MatchString(env.Value)
+	return accept
 }
--- internal/filter_test.go
+++ internal/filter_test.go
@@ -1,6 +1,9 @@
 package internal
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+)
 
 func TestNoPrefixFilter_Accept(t *testing.T) {
 	f := NewNoPrefixFilter("_")
@@ -35,7 +38,45 @@ func TestNoPrefixFilter_Accept(t *testing.T) {
 		if act != tt.exp {
 			t.Errorf("Wrong Accept result. Got %t for %+v", act, tt.input)
 		}
+	}
+}
 
+func TestRegexFilter_Accept(t *testing.T) {
+	regex, err := regexp.Compile(".*WHAT.*")
+	if err != nil {
+		t.Fatal(err)
+	}
+	f := RegexFilter{regex: regex}
+	tests := []struct {
+		input EnvVar
+		exp   bool
+	}{
+		{
+			input: EnvVar{
+				Name:  "_SOMEWHATVAR",
+				Value: "value",
+			},
+			exp: true,
+		},
+		{
+			input: EnvVar{
+				Name:  "SOMEVAR",
+				Value: "valWHATue",
+			},
+			exp: true,
+		}, {
+			input: EnvVar{
+				Name:  "SOMEVAR",
+				Value: "_value",
+			},
+			exp: false,
+		},
 	}
 
+	for _, tt := range tests {
+		act := f.Accept(tt.input)
+		if act != tt.exp {
+			t.Errorf("Wrong Accept result. Got %t for %+v", act, tt.input)
+		}
+	}
 }
